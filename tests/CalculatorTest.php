f5252e8b0b53660e47f6fd817d7c5e0575256d97
joaovictor-ip4y <joao.victor@ip4y.com.br>
2025-01-22 13:27:04 -0300

    teste

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -15,38 +15,30 @@ class CalculatorTest extends TestCase
         $this->calculator = new Calculator();
     }
 
-    // Teste que vai passar
     public function testAdd()
     {
         $result = $this->calculator->add(2, 3);
         $this->assertEquals(5, $result);
     }
 
-    // Teste que vai falhar
     public function testSubtract()
     {
         $result = $this->calculator->subtract(5, 3);
-        // Aqui vamos fazer o teste falhar propositalmente
-        $this->assertEquals(10, $result); // Esperado 10, mas o cálculo correto é 2
+        $this->assertEquals(2, $result);
     }
 
-    // Teste que vai ser pulado
-    /**
-     * @testdox This test will be skipped
-     * @doesNotPerformAssertions
-     */
     public function testMultiply()
     {
-        $this->markTestSkipped("This test is skipped intentionally");
+        $result = $this->calculator->multiply(3, 4);
+        $this->assertEquals(12, $result);
     }
 
-    // Teste para dividir
     public function testDivide()
     {
         $result = $this->calculator->divide(10, 2);
         $this->assertEquals(5, $result);
 
-        // Test division by zero - será falho
+        // Test division by zero
         $this->expectException(\DivisionByZeroError::class);
         $this->calculator->divide(10, 0);
     }
