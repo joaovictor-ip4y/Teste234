32ada9ad65a9396235b56490c3135b13cf0dca42
joaovictor-ip4y <joao.victor@ip4y.com.br>
2025-01-23 10:57:54 -0300

    teste7

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -18,4 +18,37 @@ class CalculatorTest extends TestCase
         $calculator = new Calculator();
         $this->assertEquals(1, $calculator->subtract(3, 2));
     }
+
+    public function testMultiply(): void
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(6, $calculator->multiply(2, 3));
+    }
+
+    public function testDivide(): void
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(2, $calculator->divide(6, 3));
+    }
+
+    public function testDivideByZero(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage("Division by zero is not allowed.");
+
+        $calculator = new Calculator();
+        $calculator->divide(6, 0);
+    }
+
+    public function testModulo(): void
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(1, $calculator->modulo(7, 3));
+    }
+
+    public function testPower(): void
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(8, $calculator->power(2, 3));
+    }
 }
